e68a16d36439d6688b66f490d634e9497cf3c136
Christian Barrios <christiandbf@hotmail.com>
2017-05-13 11:02:14 -0500

    Finished API

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
-function getLocation() {
-    $.getJSON("http://ip-api.com/json", function(data) {
-        $("#location").append(data.city + ", " + data.country);
-    });
-}
-    
 $(document).ready(function() {
-    getLocation();
-    var location = document.getElementById("location").textContent;
-    
+    var locationJSON;    
+    $.ajax({
+        url : 'http://ip-api.com/json',
+        type : 'GET',
+        dataType : 'json',
+        async: false,
+        success : function(json) {
+            locationJSON = json;
+        }, 
+        error : function(xhr, status) {
+            alert('Disculpe, error determinando su ubicación');
+        }, 
+    });
+    var urlApiWeather = "http://api.openweathermap.org/data/2.5/weather?q=" + locationJSON.city + "&APPID=bb36ece062396394916c3426efa588e4";
+    $.getJSON(urlApiWeather, function(params) {
+        $("#location").append(locationJSON.city + ", " + locationJSON.country);
+        $("#location-weather").append(params.main.temp);
+        console.log(params, locationJSON);
+    });
 });
